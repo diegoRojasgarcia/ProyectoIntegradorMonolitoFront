8effe117874c0619513024c2cd08de64db918a01
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-10-11 18:56:11 -0300

    update NavBar and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
-  <footer className="bg-gray-800 shadow text-white ">
-    <div className="w-full mx-auto p-4 md:flex md:items-center md:justify-between ">
+  <footer className="bg-gray-800 shadow text-white text-center h-24">
+    <div className=" pt-9">
       <span className="text-sm text-gray-200 sm:text-center dark:text-gray-200 ">
         © 2023{" "}
         <a href="#" className="hover:underline">
@@ -8,28 +8,6 @@ const Footer = () => (
         </a>
         . All Rights Reserved.
       </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6 ">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6">
-            Privacy Policy
-          </a>
-        </li>
-        <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6">
-            Licensing
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:underline">
-            Contact
-          </a>
-        </li>
-      </ul>
     </div>
   </footer>
 );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,9 +4,9 @@ export default function Navbar({ fixed }: any) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="fixed flex w-full flex-wrap items-center justify-between bg-gray-800 shadow py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
+      {/* <nav className="fixed flex w-full flex-wrap items-center justify-between bg-gray-800 shadow py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
         <div className="flex w-full flex-wrap items-center justify-end px-3">
-          <div className="relative flex items-center ">
+          <div className="relative flex items-center">
             <a
               className="ml-1 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400 "
               href="#"
@@ -25,6 +25,67 @@ export default function Navbar({ fixed }: any) {
             <span className="ml-3 rounded-md bg-danger px-[0.85em] py-[0.6em] text-[0.6rem] font-bold leading-none text-white">
               1
             </span>
+          </div>
+        </div>
+        <div className="mx-auto lg:max-w-7xl bg-gray-200">
+          <div className="relative flex items-center lg:max-w-7xl">hola</div>
+        </div>
+      </nav> */}
+      <nav className="bg-gray-800 text-white">
+        <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="#" className="flex items-center">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+              <h1>Burgir</h1>
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className="hidden w-full md:block md:w-auto"
+            id="navbar-default "
+          >
+            <div className="relative flex items-center">
+              <a
+                className="ml-1 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400 "
+                href="#"
+              >
+                <span className="[&>svg]:w-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  </svg>
+                </span>
+              </a>
+              <span className="ml-3 rounded-md bg-danger px-[0.85em] py-[0.6em] text-[0.6rem] font-bold leading-none text-white">
+                1
+              </span>
+            </div>
           </div>
         </div>
       </nav>
